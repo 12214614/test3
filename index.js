016bf9697893e15f0c9955859507f2be7038354c
ESHWARTEJA AELAGONDA_12214614 <118893267+12214614@users.noreply.github.com>
2025-09-27 18:47:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const name = document.getElementById("greet")
+const color = document.getElementById("red")
+const color1 = document.getElementById("blue")
+const color2 = document.getElementById("green")
+const color3 = document.getElementById("yellow")
+let txt = document.getElementById("txt")
+
+function red(){
+color.style.backgroundColor = "red"
+}
+function blue(){
+color1.style.backgroundColor = "blue"
+txt.style.color= 'blue'
+
+  
+}
+
+function green(){
+color2.style.backgroundColor = "green"
+txt.style.color= 'green'
+}
+function yellow(){
+color3.style.backgroundColor = "yellow"
+
+}
+
+let txt1;
+
+function btn(){
+txt1 = txt.value;   
+  greet.innerText = "Hello , " + txt1;
+}
+function btn1(){
+  txt.style.color = 'black'
+}
+function handleDoubleClick() {
+    txt.style.color = 'black'
+}
+color1.addEventListener("dblclick", handleDoubleClick);
+console.log(handleDoubleClick)
